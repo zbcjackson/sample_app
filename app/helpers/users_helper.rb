4834449562cd80a3d81d61aac50cee4cbb838e56
Jackson Zhang <zbcjackson@gmail.com>
2013-07-31 17:17:54 +0800

    Finish user edit, update, index, and destroy actions

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,10 @@
 module UsersHelper
 
   # Returns the Gravatar (http://gravatar.com/) for the given user.
-  def gravatar_for(user)
+  def gravatar_for(user, options={})
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    options = {alt: user.name, class: "gravatar"}.merge options
+    image_tag(gravatar_url, options)
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,8 +16,19 @@ describe User do
 	it { should respond_to(:password_confirmation) }
 	it { should respond_to(:remember_token) }
 	it { should respond_to(:authenticate) }
+	it { should respond_to(:admin) }
 
 	it { should be_valid }
+	it { should_not be_admin }
+
+	describe "with admin attribute set to 'true'" do
+		before do
+			@user.save!
+			@user.toggle!(:admin)
+		end
+
+		it { should be_admin }
+	end
 
 	describe "when name is not present" do
 		before { @user.name = " " }
@@ -72,29 +83,29 @@ describe User do
 		it { should_not be_valid }
 	end
 
-	  describe "with a password that's too short" do
-    before { @user.password = @user.password_confirmation = "a" * 5 }
-    it { should be_invalid }
-  end
+	describe "with a password that's too short" do
+		before { @user.password = @user.password_confirmation = "a" * 5 }
+		it { should be_invalid }
+	end
 
-  describe "return value of authenticate method" do
-    before { @user.save }
-    let(:found_user) { User.find_by(email: @user.email) }
+	describe "return value of authenticate method" do
+		before { @user.save }
+		let(:found_user) { User.find_by(email: @user.email) }
 
-    describe "with valid password" do
-      it { should eq found_user.authenticate(@user.password) }
-    end
+		describe "with valid password" do
+			it { should eq found_user.authenticate(@user.password) }
+		end
 
-    describe "with invalid password" do
-      let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+		describe "with invalid password" do
+			let(:user_for_invalid_password) { found_user.authenticate("invalid") }
 
-      it { should_not eq user_for_invalid_password }
-      specify { expect(user_for_invalid_password).to be_false }
-    end
-  end
+			it { should_not eq user_for_invalid_password }
+			specify { expect(user_for_invalid_password).to be_false }
+		end
+	end
 
-  describe "remember token" do
-    before { @user.save }
-    its(:remember_token) { should_not be_blank }
-  end
+	describe "remember token" do
+		before { @user.save }
+		its(:remember_token) { should_not be_blank }
+	end
 end
